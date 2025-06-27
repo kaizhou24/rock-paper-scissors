ec8ec23fabd8ca403a3e489d347589d31da43f4d
kaizhou24 <130264951+kaizhou24@users.noreply.github.com>
2025-06-27 15:47:51 -0400

    Add random computer choice

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ Write code that tracks score.
 Write code to determine the winner of a single round.
 Write code to determine the winner of the entire game.
 */
+
+function getComputerChoice(){
+   switch(Math.floor(Math.random() * 3) + 1){
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+   }
+}
