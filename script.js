d4837877942c090fba6cba021649aac27a8b5572
kaizhou24 <130264951+kaizhou24@users.noreply.github.com>
2025-06-27 15:39:20 -0400

    Link script.js to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+/*
+Write code that randomly returns rock, paper, or scissors.
+Write code that allows users to make a choice.
+Write code that tracks score.
+Write code to determine the winner of a single round.
+Write code to determine the winner of the entire game.
+*/
